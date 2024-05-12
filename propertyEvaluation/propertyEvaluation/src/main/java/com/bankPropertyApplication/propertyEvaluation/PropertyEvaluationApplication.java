c884977a7f9f091e2b2bac4add76dc4440a8ee37
Sandeep Girase <sandeepgirase55@gmail.com>
2024-05-12 13:49:40 +0530

    First Commit

---
+++ propertyEvaluation/propertyEvaluation/src/main/java/com/bankPropertyApplication/propertyEvaluation/PropertyEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.bankPropertyApplication.propertyEvaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyEvaluationApplication.class, args);
+	}
+
+}
---
+++ propertyEvaluation/propertyEvaluation/src/test/java/com/bankPropertyApplication/propertyEvaluation/PropertyEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankPropertyApplication.propertyEvaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
